4de9abd33aa831da95aaad13bc77659ec78675af
Rawan Mohamed <rawanmohamedhassan2001@gmail.com>
2023-11-08 11:32:39 -0600

    added latest changes to clinic

--- frontend/src/Pages/LoginPageclinic.jsx
+++ frontend/src/Pages/LoginPageclinic.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import img from '../Components/Logo/img.png';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -26,19 +27,86 @@ const buttonContainerStyle = {
   marginTop: '10px',
 };
 
+// function LoginPage() {
+//   const navigate = useNavigate();
+
+//   const handlePatientRegistration = () => {
+//     navigate('/patientreg');
+//   };
+
+//   const handleLogin = () => {
+//     navigate('/admin-home');
+//   };
+
+//   const handleDoctorRegistration = () => {
+//     navigate('/docreg');
+//   };
+
+
 function LoginPage() {
   const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleDoctorRegistration = () => {
+    navigate('/docreg');
+  };
 
   const handlePatientRegistration = () => {
     navigate('/patientreg');
   };
 
-  const handleLogin = () => {
-    navigate('/admin-home');
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
   };
 
-  const handleDoctorRegistration = () => {
-    navigate('/docreg');
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/login', {
+        username,
+        password,
+      });
+
+      const data = response.data;
+
+      if (data.success) {
+        // Use the role from the response to determine where to navigate
+        switch (data.role) {
+          case 'Admin':
+            navigate('/admin-home');
+            break;
+          case 'Doctor':
+            navigate('/doc-home/:id');
+            break;
+          case 'Patient':
+            navigate('/clinic-patient-home/:id');
+            break;
+        
+          default:
+            // Handle any user types that don't have a specified route
+            setError('User role is not recognized.');
+            break;
+        }
+      } else {
+        // If the login was not successful, but there was no error thrown
+        setError(data.message);
+      }
+    } catch (error) {
+      console.error('Login error', error);
+      if (error.response) {
+        // If the server responded with a status other than 2xx, get the message from the server's response
+        setError(error.response.data.message);
+      } else {
+        // If the server did not respond or there was a different error, set a generic error message
+        setError('An error occurred during login. Please try again.');
+      }
+    }
   };
 
   return (
@@ -56,6 +124,8 @@ function LoginPage() {
             label="Username"
             variant="standard"
             fullWidth
+            value={username}
+            onChange={handleUsernameChange}
           />
         </div>
         <div style={textfieldStyle}>
@@ -63,6 +133,8 @@ function LoginPage() {
             label="Password"
             variant="standard"
             fullWidth
+            value={password}
+            onChange={handlePasswordChange}
           />
         </div>
         <div style={buttonContainerStyle}>
